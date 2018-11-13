a7290aaed346de4b1875f0f47b8e3f53a9fbd1f9
ivanx5667 <44973371+ivanx5667@users.noreply.github.com>
2018-11-13 11:33:22 +0300

    Update index.php

--- index.php
+++ index.php
@@ -38,11 +38,12 @@ if($method == 'POST'){
 	$response = new \stdClass();
 	$response->fulfillmentText = $speech;
 	
-	$response->speech = $speech;
-	$response->displayText = $speech;
-	$response->source = "webhook";
+	// $response->speech = $speech;
+	// $response->displayText = $speech;
+	// $response->source = "webhook";
 	
 	echo json_encode($response);
+return $response;
 }
 else
 {
